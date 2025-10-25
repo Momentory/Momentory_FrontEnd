28e2e2451a10824175ff1a1bb746faba9c7e2b09
DESKTOP-QR4UUM5\user <rmsdud0728@catholic.ac.kr>
2025-10-26 03:15:15 +0900

    chore: disable no-empty-object-type eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ export default tseslint.config(
 
       '@typescript-eslint/no-explicit-any': 'off',   // any 허용
       '@typescript-eslint/no-unused-vars': 'off',    // 안 쓰는 변수 허용
+      '@typescript-eslint/no-empty-object-type': 'off',
     },
   },
 )
