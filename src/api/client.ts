83ffebeee79b0f7b7005ab323c4d9c0c3f814dde
haeun <haeun9634@naver.com>
2025-11-11 22:53:35 +0900

    fix: '/api' 추가

--- src/api/client.ts
+++ src/api/client.ts
@@ -16,7 +16,7 @@ interface ReissueResponse {
 
 // API 클라이언트 생성
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL+"/api",
   withCredentials: true,
 });
 
